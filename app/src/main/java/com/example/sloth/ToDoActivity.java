c8a899ab026ffcdd988a3e6a242576b9091eb707
annadoerr <doerranna@t-online.de>
2020-01-18 15:32:58 +0100

    ButtonCreateToDo

--- app/src/main/java/com/example/sloth/ToDoActivity.java
+++ app/src/main/java/com/example/sloth/ToDoActivity.java
@@ -2,26 +2,17 @@ package com.example.sloth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class ToDoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_to_do);
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(R.string.listTitle);
-    }
-
-    public void addListClicked(View v) {
-        Intent intent = new Intent (this, ToDoActivity.class);
-        startActivity(intent);
-
+        getSupportActionBar().setTitle(R.string.todoTitle);
     }
 }
