37a9c1d4b83debb8fd431b20efa028c89410202a
annadoerr <doerranna@t-online.de>
2020-01-18 14:27:49 +0100

    Icon_and_Toolbar

--- app/src/main/java/com/example/sloth/MainActivity.java
+++ app/src/main/java/com/example/sloth/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sloth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Your Lists");
     }
 }
